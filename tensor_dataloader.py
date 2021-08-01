fbdc02326b93980c4407472df3903a04f8f9b806
Alex Bogun <alexbogun@gmail.com>
2021-08-02 00:00:33 +0200

    added ability to use transforms

--- tensor_dataloader.py
+++ tensor_dataloader.py
@@ -59,11 +59,71 @@ class FastTensorDataLoader:
                 raise StopIteration
             if self.indices is not None:
                 indices = self.indices[self.i:self.i+self.batch_size]
-                batch = tuple(torch.index_select(t, 0, indices) for t in self.tensors)
+                batch = self.dataset[indices]
             else:
-                batch = tuple(t[self.i:self.i+self.batch_size] for t in self.tensors)
+                batch = self.dataset[self.i:self.i+self.batch_size]
             self.i += self.batch_size
             return batch
 
     def __len__(self):
         return self.n_batches
+
+
+class FastTensorDataset(torch.utils.data.Dataset):
+ 
+    '''
+    A simple loading dataset - loads the tensor that are passed in input. This is the same as
+    torch.utils.data.TensorDataset except that you can add transformations to your data and target tensor.
+    Target tensor can also be None, in which case it is not returned.
+    '''
+ 
+    def __init__(self, data_tensor, target_tensor=None, transform=None, target_transform=None):
+        if target_tensor is not None:
+            assert data_tensor.size(0) == target_tensor.size(0)
+        self.data_tensor = data_tensor
+        self.target_tensor = target_tensor
+        self.tensors = [data_tensor, target_tensor]
+ 
+        if transform is None:
+            transform = []
+        if target_transform is None:
+            target_transform = []
+ 
+        if not isinstance(transform, list):
+            transform = [transform]
+        if not isinstance(target_transform, list):
+            target_transform = [target_transform]
+ 
+        self.transform = transform
+        self.target_transform = target_transform
+ 
+    def __getitem__(self, index):
+ 
+        data_tensor = self.data_tensor[index]
+        for transform in self.transform:
+            data_tensor = transform(data_tensor)
+ 
+        if self.target_tensor is None:
+            return data_tensor
+ 
+        target_tensor = self.target_tensor[index]
+        for transform in self.target_transform:
+            target_tensor = transform(target_tensor)
+ 
+        return data_tensor, target_tensor
+ 
+    def __len__(self):
+        return self.data_tensor.size(0)
+
+
+def get_tensor_dataset(dataset, transform=None, target_transform=None, cuda=1):
+    X, Y = [],[]
+    for x, y in dataset:
+        X.append(x)
+        Y.append(y)
+    X = torch.stack(X)
+    Y = torch.tensor(Y)
+
+    if cuda:
+        X, Y = X.to('cuda'), Y.to('cuda')
+    return  FastTensorDataset(X, Y, transform, target_transform)
